5c7a7da1e1c637ef852e4ba2ecf168636d91578b
Nicola Girardi <nicola@aloc.in>
2020-01-05 22:46:36 +0000

    all: fix for 32c9b77a: add files that should have been included

---
+++ cmd/musclefs/musclefs_posix.go
@@ -0,0 +1,21 @@
+// +build !plan9
+
+package main
+
+import (
+	"github.com/google/gops/agent"
+	"github.com/nicolagi/go9p/p"
+	log "github.com/sirupsen/logrus"
+)
+
+func usersPool() p.Users {
+	return p.OsUsers
+}
+
+func gopsListen() {
+	if err := agent.Listen(agent.Options{
+		ShutdownCleanup: true,
+	}); err != nil {
+		log.Warningf("Could not start gops agent: %v", err)
+	}
+}
---
+++ tree/node_posix.go
@@ -0,0 +1,14 @@
+// +build !plan9
+
+package tree
+
+import (
+	"os"
+
+	"github.com/nicolagi/go9p/p"
+)
+
+var (
+	nodeUID = p.OsUsers.Uid2User(os.Geteuid()).Name()
+	nodeGID = p.OsUsers.Gid2Group(os.Getegid()).Name()
+)
